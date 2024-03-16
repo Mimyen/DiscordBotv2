1fd716ef212caf6056f9b238e656498ad7e9028f
Mimyen <michalpazdan2329@gmail.com>
2024-03-16 20:18:04 +0100

    .

--- DiscordBot/Bot/Config/config.py
+++ DiscordBot/Bot/Config/config.py
@@ -141,8 +141,7 @@ CRYPTO_PATH = "Temp/cryptocurrency/"
 
 
 # ManagerApp Extension
-MANAGER_IP = '192.168.1.11'
-#MANAGER_IP = '127.0.0.1'
+MANAGER_IP = '127.0.0.1'
 MANAGER_PORT = 2223
 
 MANAGER_PASSWORD = "pawel&ola=dobrePolacz3nie"
